32f4844e45b08d33add6e5930cc2cd247b73146b
corrumptus <lazarinilucas42@gmail.com>
2024-04-03 22:57:24 -0300

    refactoring the name of the menu into the correct one

--- app/src/main/java/lazarini/lucas/intent/MainActivity.kt
+++ app/src/main/java/lazarini/lucas/intent/MainActivity.kt
@@ -73,7 +73,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?):Boolean{
-        menuInflater.inflate(R.menu.menu_main,menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 }
