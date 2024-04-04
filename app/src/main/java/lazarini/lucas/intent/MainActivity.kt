5b5e8682262067dbd82bd237d14e57bb2e117da1
corrumptus <lazarinilucas42@gmail.com>
2024-04-03 22:24:05 -0300

    setting the actionBar

--- app/src/main/java/lazarini/lucas/intent/MainActivity.kt
+++ app/src/main/java/lazarini/lucas/intent/MainActivity.kt
@@ -1,5 +1,6 @@
 package lazarini.lucas.intent
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import lazarini.lucas.intent.databinding.ActivityMainBinding
@@ -12,5 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        amb.mainTb.apply {
+            title = getString(R.string.app_name)
+            subtitle = this@MainActivity.javaClass.simpleName
+            setSupportActionBar(this)
+        }
     }
 }
