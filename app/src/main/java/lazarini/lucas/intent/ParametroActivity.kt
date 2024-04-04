9177adeba64a7644fe9e8445f5c975e0ceade6d6
corrumptus <lazarinilucas42@gmail.com>
2024-04-03 22:39:52 -0300

    inflating the parametro activity layout

--- app/src/main/java/lazarini/lucas/intent/ParametroActivity.kt
+++ app/src/main/java/lazarini/lucas/intent/ParametroActivity.kt
@@ -1,4 +1,16 @@
 package lazarini.lucas.intent
 
-class ParametroActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import lazarini.lucas.intent.databinding.ActivityParametroBinding
+
+class ParametroActivity : AppCompatActivity() {
+    private val apb: ActivityParametroBinding by lazy {
+        ActivityParametroBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(apb.root)
+    }
 }
